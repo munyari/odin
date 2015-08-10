7d1897175417fbccafef42472fed95fc556fe0f8
Panashe M. Fundira <fundirap@gmail.com>
2015-08-10 19:21:29 -0400

    Consolidate bubble sort and bubble sort by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,5 +18,26 @@ def bubble_sort(arr)
 end
 
 
-
 p bubble_sort([4,3,78,2,0,2])
+
+
+def bubble_sort_by(arr)
+  n = arr.length
+  loop do
+    swapped = false
+    for i in 1..(n-1)
+      if (yield(arr[i - 1], arr[i])) > 0
+        # swap arr[i-1] and arr[i]
+        temp = arr[i-1]
+        arr[i-1] = arr[i]
+        arr[i] = temp
+        swapped = true
+      end
+    end
+    n -= 1
+    break if !swapped
+  end
+  arr
+end
+
+p bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length }

3e3897c6edc42c3bcc0193d8021b56a40dd0beae
Panashe M. Fundira <fundirap@gmail.com>
2015-11-15 17:06:57 -0500

    server and client project

--- learning-ruby/web-server/browser.rb
+++ learning-ruby/web-server/browser.rb
@@ -1,16 +1,47 @@
-require 'socket'
+#!/usr/bin/env ruby
 
-host = 'www.tutorialspoint.com' # The web server
-port = 80                       # Default HTTP port
-path = '/index.htm'             # The file we want
+require "socket"
+require "json"
 
-# This is the HTTP request we send to fetch the file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+#host = "www.google.com" # The web server
+host = "localhost"                # The web server
+port = 2000                       # Default HTTP port
+
+req_type = nil
+until [1, 2].include?(req_type)
+  puts "Would you like to send a GET [1] or a POST [2] request?"
+  req_type = gets.to_i
+end
+
+if req_type == 1
+  path = "/index.html"             # The file we want
+
+  # This is the HTTP request we send to fetch the file
+  request = "GET #{path} HTTP/1.1\r\n\r\n"
+
+
+else
+  puts "Hello loyal viking brother! Before embarking on your first raid, you\n
+  must enlist by providing your name and email address"
+  print "name: "
+  name = gets.chomp
+  print "email: "
+  email = gets.chomp
+  form_data = {:viking => {:name=>name, :email=>email}}
+  json = form_data.to_json
+  request = "POST /thanks.html HTTP/1.1\n"
+  request += "From: user@example.com\n"
+  request += "User-Agent: OdinBrowser/1.0\n"
+  request += "Content-Type: application/x-www-urlencoded\n"
+  request += "Content-Length: #{json.bytesize}\r\n\r\n"
+  request += "#{json}\r\n\r\n"
+end
 
 socket = TCPSocket.open(host, port) # Connect to server
 socket.print(request)               # Send request
 response = socket.read              # Read complete response
-#Split response at first blank line into headers and body
-headers, body = response.split("\r\n\r\n", 2)
-print body
 
+# Split response at first blank line into headers and body
+headers, body = response.split("\r\n\r\n", 2)
+#puts body
+puts response
--- learning-ruby/web-server/server.rb
+++ learning-ruby/web-server/server.rb
@@ -1,9 +1,118 @@
-require 'socket'
+#!/usr/bin/env ruby
 
-server = TCP.open(2000)
+require "socket"
+require "uri"
+require "json"
+
+# Files will be served from this directory
+WEB_ROOT = "/home/nash/code/odin/learning-ruby/web-server"
+
+# Map extensions to their content type
+CONTENT_TYPE_MAPPING = {
+  "html" => "text/html",
+  "txt" => "text/plain",
+  "png" => "image/png",
+  "jpg" => "image/jpeg"
+}
+
+# Treat as binary data if content type cannot be found
+DEFAULT_CONTENT_TYPE = "application/octet-stream"
+
+# This helper function parses the extension of the
+# requested file and then looks up its content type.
+#
+def content_type(path)
+  ext = File.extname(path).split(".").last
+  CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+end
+
+# Takes a request line (e.g. "GET /path?foo=bar HTTP/1.1")
+# and extracts the path from it, scrubbing out parameters
+# and unescaping URI-encoding
+#
+# This cleaned up path (e.g. "/path") is then converted into a relative path 
+# to a file in the server"s public folder by joining it with the WEB_ROOT and
+# sanitized to keep the path strictly in the public folder
+def requested_file(request)
+  request_uri = request.split(" ")[1]
+  path = URI.unescape(URI(request_uri).path)
+
+  clean = []
+
+  # Split the path into components
+  parts = path.split("/")
+
+  parts.each do |part|
+    # skip any empty or current current directory (".") path components
+    next if part.empty? || part == "."
+    # If the path component goes up one directory level (".."),
+    # remove the last clean component
+    # Otherwise, add the component to the Array of clean components
+    part == ".." ? clean.pop : clean << part
+  end
+
+  # return the web root joined to the client path
+
+  File.join(WEB_ROOT, *clean)
+end
+
+server = TCPServer.new("localhost", 2000)
+
 loop do
-  client = server.accept
-  client.puts(Time.now.ctime)
-  client.puts "Closing the connection. Bye!"
-  client.close
+  socket = server.accept
+
+  request = socket.gets
+
+  STDERR.puts request
+  # GET or POST
+  req_type = /(?:^|(?:[.!?]\s))(\w+)/.match(request)[0]
+
+  if req_type == "GET"
+    path = requested_file(request)
+    path = File.join(path, "index.html") if File.directory?(path)
+
+    if File.exist?(path) && !File.directory?(path)
+      File.open(path, "rb") do |file|
+        socket.print "HTTP/1.1 200 OK\r\n" +
+          "Content-Type: #{content_type(file)}\r\n" +
+        "Content-Length: #{file.size}\r\n" +
+        "Connection: close\r\n"
+
+        socket.print "\r\n"
+
+        IO.copy_stream(file, socket)
+      end
+    else
+      message = "File not found\n"
+
+      socket.print "HTTP/1.1 404 Not Found\r\n" +
+        "Content-Type: text/plain\r\n" +
+        "Content-Length: #{message.size}\r\n" +
+      "Connection: close\r\n"
+
+      socket.print "\r\n"
+
+      socket.print message
+    end
+  elsif req_type == "POST"
+    path = requested_file(request)
+    path = File.join(path, "index.html") if File.directory?(path)
+    file = File.open(path)
+    # VERY hackish way to get the JSON line
+    5.times { socket.gets }
+    form_json = socket.gets
+    form_hash = JSON.parse(form_json)
+    html_str = ""
+    form_hash.each_pair do |k, v|
+      v.each_pair do |attribute, value|
+        html_str += "<li>#{attribute}: #{value}</li>"
+      end
+    end
+    socket.puts file.read.gsub("<%= yield %>", html_str)
+    file.close
+
+  end
+
+  socket.close
+
 end
